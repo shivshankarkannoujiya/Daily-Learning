3678664c1b7d145808b6fd9a82b30980f473a121
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-08-31 04:32:08 +0530

    Add initial implementation of student profile and weather condition handling

---
+++ Day-8/Chapter_1.js
@@ -0,0 +1,26 @@
+// string: "", '', ``
+let name = "Rohan";
+
+let age = 23;
+let score = 90.7; // float
+
+let isLoggedIn = true;
+
+let favouriteClass = null;
+
+let homeTown;
+let homeTown_ = undefined;
+
+let skills = ["python", "javascript", "Nodejs", true, false, 12];
+
+let studentProfile = {
+  name: "Rohan",
+  age: 23,
+  isLoggedIn: true,
+  isPaid: true,
+  skills: ["html", "css", "javscript"],
+  favouriteClass: null,
+};
+
+typeof favouriteClass;
+// TODO: Check the typeof function => object
---
+++ Day-8/Chapter_2.js
@@ -0,0 +1,12 @@
+// We are doing server call to get weather information
+// Returns: sunny, rainy, cloudy
+
+let weather = "rainy";
+
+if (weather === "rainy") {
+  console.log(`its raining, Carry umbrella with you`);
+} else if (weather === "cloudy") {
+  console.log(`Its cloudy, Carry jacket with you`);
+} else {
+  console.log(`Chill the sunny day`);
+}
