7424a7795a67636c5ccd2aa5e16fd377f5cadec6
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-09-01 05:41:38 +0530

    Add implementation of object reference and mutation example

---
+++ Day-10/Chapter_2.js
@@ -0,0 +1,20 @@
+// let fanme = "Abhishek"
+// let fanme2 = fanme
+
+// console.log(`Fname: ${fanme}`);
+// console.log(`Fname2: ${fanme2}`);
+
+// fanme = "Raman"
+// fanme2 = "Rohan"
+// console.log(`Fname: ${fanme}`);
+// console.log(`Fname2: ${fanme2}`);
+
+let p1 = {
+  fname: "Abhishek",
+};
+
+let p2 = p1; 
+
+p1.fname = "Rohan";
+console.log(p2);
+console.log(1);
