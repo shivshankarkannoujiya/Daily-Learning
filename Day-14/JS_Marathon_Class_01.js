8fb372bed1b9af3a20e0badbbe682d3d4fc9a29c
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-09-05 12:13:10 +0530

    Add JavaScript file with variable declarations and basic operations

---
+++ Day-14/JS_Marathon_Class_01.js
@@ -0,0 +1,45 @@
+let name = "Piyush"
+let pi = 3.14
+
+let number = 12
+let text = "hello"
+let isTrue = true
+let nothing = null
+let undefinedVar = undefined
+let bestClass;
+let symbolVar = Symbol()
+
+console.log(typeof undefinedVar);
+console.log(typeof bestClass);
+console.log(typeof symbolVar);
+
+
+let person = {
+    name: "Jay",
+    age: 23,
+    isStudent: true
+}
+
+let num = "34"
+let convertedNumber = Number(num)
+let convertedNumber_2 = +num
+
+let str = 123
+let convertedString = String(str)
+
+let firstname = "Piyush"
+let lastname = "Garg"
+
+// let fullName = firstname + lastname //PiyushGarg
+let fullName = firstname + " " + lastname //Piyush Garg
+
+let messages = "Hello World"
+
+console.log(messages.toUpperCase());
+console.log(messages.indexOf("World"));
+console.log(messages.slice(0, 4));
+
+
+
+
+
