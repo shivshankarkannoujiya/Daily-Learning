711ef02ad1136ceaf64d96bce1a4e5a5484a562c
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-09-11 14:35:50 +0530

    Add fetch resposne and render on screen

--- Day-15/Promises/script.js
+++ Day-15/Promises/script.js
@@ -1,17 +1,28 @@
-console.log(`HELLO`);
-const data = fetch(`https://api.freeapi.app/api/v1/public/randomproducts`);
-console.log(`Fetch initiated, got Promise: `, data);
-data
-  .then((res) => {
-    console.log(`Fetched Successfully, Parsing JSON...`);
-    console.log(typeof res);
-    return res.json();
-  })
-  .then((jsonDATA) => {
-    console.log(typeof jsonDATA);
-    console.log(`Final API DATA Received: `, jsonDATA);
-  })
-  .catch((error) => console.error(`Oops! Something went wrong: `, error))
-  .finally(() => {
-    console.log(`Process Finished`);
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  const productsContainer = document.getElementById("productsContainer");
+
+  const data = fetch(`https://api.freeapi.app/api/v1/public/randomproducts`);
+  console.log(`Fetch initiated, got Promise: `, data);
+  data
+    .then((res) => {
+      console.log(`Fetched Successfully, Parsing JSON...`);
+      console.log(typeof res);
+      return res.json();
+    })
+    .then((parsedData) => {
+      console.log(typeof parsedData);
+
+      console.log(`Final API DATA Received: `, parsedData);
+      console.log(parsedData.data.data);
+      const products = parsedData.data.data;
+      products.forEach((product) => {
+        const li = document.createElement("li");
+        li.textContent = product.title;
+        productsContainer.appendChild(li);
+      });
+    })
+    .catch((error) => console.error(`Oops! Something went wrong: `, error))
+    .finally(() => {
+      console.log(`Process Finished`);
+    });
+});
