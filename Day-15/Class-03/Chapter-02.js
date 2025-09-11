cd5626769798d896a7a1af166dd6173e5792ff7c
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-09-11 09:44:00 +0530

    enables negative indexing

---
+++ Day-15/Class-03/Chapter-02.js
@@ -0,0 +1,33 @@
+function negativeIndex(arr){
+    return new Proxy(arr, {
+        get(target, prop) {
+            const index = Number(prop)
+            if (index < 0) {
+                return target[target.length + index]
+            }
+            return target[index]
+        },
+
+        set(target, prop, value) {
+            const index = Number(prop);
+            if (index < 0) {
+                target[target.length + index] = value
+            }
+            target[index] = value;
+            return true;
+        }
+    })
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+arr = negativeIndex(arr)
+console.log(arr[-1]);
+
+arr[-1] = 22
+console.log(arr[-1]);
+
+
+
+
+
+
