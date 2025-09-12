053269c653232467c505d4d33a9e37010de6b439
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-09-12 08:53:34 +0530

    Uncomment asynchronous file reading logic and improve sum function with a delay

--- Day-15/Class-04/index.js
+++ Day-15/Class-04/index.js
@@ -6,20 +6,22 @@ import fs from "fs";
 // console.log(`Program end...`);
 
 
-// Using Callback Async
-// console.log(`Starting program....`)
-// fs.readFile(`./hello.txt`, `utf-8`, function (err, content) {
-//     if (err) {
-//         console.log(`ERROR WHILE READING FILE: `, err)
-//     }
-//     console.log(`File reading success: `, content)
-// })
-// console.log(`Program end...`);
+// Using Callback: Async
+console.log(`Starting program....`)
+fs.readFile(`./hello.txt`, `utf-8`, function (err, content) {
+    if (err) {
+        console.log(`ERROR WHILE READING FILE: `, err)
+    }
+    console.log(`File reading success: `, content)
+})
+console.log(`Program end...`);
 
 
 
 function sum(a, b, Callback) {
-    Callback(a + b)
+    setTimeout(() => {
+        Callback(a + b);
+    }, 5 * 1000)
 }
 
 sum(1, 7, (value) => {
