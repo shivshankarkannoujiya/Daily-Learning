5642845941d7b92bbed416b36b636905e8862ce8
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-09-12 09:30:02 +0530

    Add callback hell

--- Day-15/Class-04/index.js
+++ Day-15/Class-04/index.js
@@ -7,23 +7,51 @@ import fs from "fs";
 
 
 // Using Callback: Async
-console.log(`Starting program....`)
-fs.readFile(`./hello.txt`, `utf-8`, function (err, content) {
+// console.log(`Starting program....`)
+// fs.readFile(`./hello.txt`, `utf-8`, function (err, content) {
+//     if (err) {
+//         console.log(`ERROR WHILE READING FILE: `, err)
+//     }
+//     console.log(`File reading success: `, content)
+// })
+// console.log(`Program end...`);
+
+
+
+// function sum(a, b, Callback) {
+//     setTimeout(() => {
+//         Callback(a + b);
+//     }, 5 * 1000)
+// }
+
+// sum(1, 7, (value) => {
+//     console.log(`SUM IS: ${value}`)
+// })
+
+
+// TODO:
+// 1. Read the file content from hello.txt
+// 2. Then create a new file backup.txt
+// 3. Copy the contents of the hello file to backup file
+// 4. delete the hello file
+
+fs.readFile("./hello.txt", "utf-8", function (err, content) {
     if (err) {
-        console.log(`ERROR WHILE READING FILE: `, err)
+        console.log("ERROR: ", err)
+    } else {
+        console.log("File Read success: ", content);
+        fs.writeFile("backup.txt", content, function (err) {
+            if (err) {
+                console.log("ERROR IN WRITTING backup.txt:", err)
+            } else {
+                fs.unlink("./hello.txt", function (err) {
+                    if (err) {
+                        console.log("ERROR WHILE DELETING FILE: ", err)
+                    } else {
+                        console.log("File deleted successfully...")
+                    }
+                })
+            }
+        })
     }
-    console.log(`File reading success: `, content)
-})
-console.log(`Program end...`);
-
-
-
-function sum(a, b, Callback) {
-    setTimeout(() => {
-        Callback(a + b);
-    }, 5 * 1000)
-}
-
-sum(1, 7, (value) => {
-    console.log(`SUM IS: ${value}`)
 })
