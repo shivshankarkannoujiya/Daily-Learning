58cc042c0b7315e02e77a0084c3da9f5478b5679
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-09-18 13:29:39 +0530

    Add debounce

---
+++ Day-18/Debounce.js
@@ -0,0 +1,25 @@
+// TODO: DEBOUNCE: Create a factory funtion that return the debounced Version of a particular function
+
+function debounce(fn, delay) {
+    let timerId;
+    return function (...args) {
+        clearTimeout(timerId) // cancel the last call
+        timerId = setTimeout(() => {
+            fn(...args)
+        }, delay)
+    }
+}
+
+const search = (query) => {
+    console.log(`Searching for: `, query)
+}
+
+searchWithDebounce = debounce(search)
+
+searchWithDebounce(`H`)
+searchWithDebounce(`Ha`)
+searchWithDebounce(`Har`)
+searchWithDebounce(`Hard`)
+searchWithDebounce(`Hard `)
+searchWithDebounce(`Hard J`)
+searchWithDebounce(`Hard Js`)
