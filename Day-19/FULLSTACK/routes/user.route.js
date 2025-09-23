0847406da0eeda985b4bdc320e586d8ad964d984
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-09-23 20:57:21 +0530

    Add password recovery and profile update routes with authentication middleware

--- Day-19/FULLSTACK/routes/user.route.js
+++ Day-19/FULLSTACK/routes/user.route.js
@@ -7,6 +7,7 @@ import {
     logoutUser,
     forgotUserPassword,
     resetUserPassword,
+    updateUserProfile,
 } from '../controllers/user.controller.js';
 import { isLoggedIn } from '../middlewares/auth,middleware.js';
 
@@ -17,5 +18,8 @@ router.put('/verify/:token', verifyUser);
 router.post('/login', loginUser);
 router.get('/me', isLoggedIn, getMe);
 router.get('/logout', isLoggedIn, logoutUser);
+router.post('/forgot-password', isLoggedIn, forgotUserPassword);
+router.post('/reset-password/:token', isLoggedIn, resetUserPassword);
+router.post('/update-profile', isLoggedIn, updateUserProfile);
 
 export default router;
